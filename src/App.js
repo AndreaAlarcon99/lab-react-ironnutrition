f58d4668d90ad800ee3a44cae90bd778e712cad7
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-10-27 10:05:29 +0200

    search done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddFoodForm from './components/AddFoodForm';
 import { Row, Divider, Button } from 'antd';
 import Search from './components/Search';
 
+const foodsCopy = [...foods]
 
 function App() {
   const [food, setFood] = useState(foods);
@@ -24,9 +25,19 @@ function App() {
         setFood(foodArray);
     }
   
-  const filterFood = () => {
-    
- }
+ const searchFoodHandler = (dish) =>{
+    const foodMod = [...foodsCopy];
+    console.log(foodMod)
+    const filteredFood = foodMod.filter(element => {
+      if (dish.length < 1) {
+        return foodMod }
+      else {
+        return element.name.toLowerCase().includes(dish.toLowerCase())
+      }
+    }) 
+    console.log(filteredFood)
+    setFood(filteredFood) 
+  }
   
   return (
     <div className="App">
@@ -34,8 +45,7 @@ function App() {
   <AddFoodForm addFood={addFoodHandler}/>
       
       <Button> Hide Form / Add New Food </Button>
-      <Search foodSearch={ filterFood} />
-
+  <Search searchFood={searchFoodHandler}/>
       <Divider>Food List</Divider>
       <Row style={{ width: '100%', justifyContent: 'center' }}>
         {food.map(food => <FoodBox  food= {food} deleteHandlerProps={deleteFood}/> )}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,16 +2,16 @@ import { Divider, Input } from 'antd';
 import { useState } from 'react';
 
 // Iteration 5
-function Search(props) {
-  const {foodSearch} = props
+function Search({searchFood}) {
+  
   
   const [query, setQuery] = useState('');
 
 
-  const searchFood = (e) => {
-    setQuery(e.target.value)
-    foodSearch(e.target.value)
-} 
+ const searchFoodBar = (e) => {
+        setQuery(e.target.value) // renderiza lo que escribo en search
+       return searchFood(e.target.value)
+    }
 
 
 
@@ -20,7 +20,7 @@ function Search(props) {
       <Divider>Search</Divider>
 
       <label >Search</label>
-      <Input  placeholder="Search by name" value={query} type="text"  onChange={searchFood}/>
+      <Input  placeholder="Search by name" value={query} type="text"  onChange={searchFoodBar}/>
     </>
   );
 }
